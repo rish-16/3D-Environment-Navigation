075208cf8f1be211fbc246373c2f7d88bfced663
rish-16 <mail.rishabh.anand@gmail.com>
2020-10-27 09:34:04 +0800

    Add LSTM MemBuff

--- src/membuffs.py
+++ src/membuffs.py
@@ -1,5 +1,15 @@
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, LSTM, Dense
 
-def get_lstm(dim):
-    pass
+def get_lstm(dim, n_actions):
+    model = Sequential()
+    model.add(Input(shape=dim))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dense(128, activation="relu"))
+    model.add(LSTM(128))
+    model.add(Dense(n_actions, activation="linear"))
+    
+    model.compile(optimizer="adam", loss="mse")
+    
+    return model
